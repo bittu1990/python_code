9dc1431589ae17992adb61b52c38decfaa7bcd3d
Kamalendu Biswas <62112081+bittu1990@users.noreply.github.com>
2020-06-23 21:39:32 +0300

    23062020

---
+++ linked_list.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, item):
+        self.item = item
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+if __name__ == __name__ == "__main__":
+    linked_list = LinkedList()
+
+    #Assign Item
+    linked_list.head = Node(1)
+    second = Node(2)
+    third = Node(3)
+
+    #Connect Nodes
+    linked_list.head.next = second
+    second.next = third
+
+    # Print the linked list item
+    while linked_list.head != None:
+        print(linked_list.head.item, end=" ")
+        linked_list.head = linked_list.head.next
